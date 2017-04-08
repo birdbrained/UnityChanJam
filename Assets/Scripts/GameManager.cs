cc883f14ae250a382ca0c7a94bc68fc6fc48699e
mwk9 <mwk9@njit.edu>
2017-04-07 20:09:56 -0400

    Added player controller and game manager, also made unitychan wave in the car

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    private static float health;
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+    public Text healthText;
+
+    // Use this for initialization
+    void Start () {
+        healthText.text = health.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        healthText.text = health.ToString("F2");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float health;
+
+	// Use this for initialization
+	void Start () 
+    {
+        GameManager.Instance.Health = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
