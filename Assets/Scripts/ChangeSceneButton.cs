999173f9883e5cf6d5fe25ee31ebf68537832e31
mwk9 <mwk9@njit.edu>
2017-04-08 14:14:03 -0400

    More changes to MainMenu, and the shutter shades can now be toggled from the MainMenu

---
+++ Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeSceneButton : MonoBehaviour 
+{
+
+    // Use this for initialization
+    void Start () 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        
+    }
+
+    public void ChangeScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
---
+++ Assets/Scripts/ExtraSwag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraSwag : MonoBehaviour 
+{
+    public static bool hasSwag;
+    private GameObject swag;
+
+    // Use this for initialization
+    void Start () 
+    {
+        swag = GameObject.Find("glasses");
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if (swag != null)
+        {
+            if (hasSwag)
+                swag.SetActive(true);
+            else
+                swag.SetActive(false);
+        }
+    }
+
+    public void ChangeSwag()
+    {
+        hasSwag = !hasSwag;
+    }
+}
