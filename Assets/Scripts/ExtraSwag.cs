6cbc99a28fb416976cbc2f2890fda69c0c83d0f7
mwk9 <mwk9@njit.edu>
2017-04-09 17:50:17 -0400

    Last fixes before the deadline!

--- Assets/Scripts/ExtraSwag.cs
+++ Assets/Scripts/ExtraSwag.cs
@@ -6,17 +6,27 @@ public class ExtraSwag : MonoBehaviour
     public static bool hasSwag;
     private GameObject swag;
 
+    [SerializeField]
+    private GameObject[] swagList;
+    public static int swagIndex = 0;
+
+
     // Use this for initialization
     void Start () 
     {
-        swag = GameObject.Find("glasses");
+        /*swag = GameObject.Find("glasses");
         if (swag != null)
         {
             if (hasSwag)
                 swag.SetActive(true);
             else
                 swag.SetActive(false);
-        }
+        }*/
+
+        foreach (GameObject sweg in swagList)
+            if (sweg != null)
+                sweg.SetActive(false);
+        ChangeSwag();
     }
 
     // Update is called once per frame
@@ -27,13 +37,31 @@ public class ExtraSwag : MonoBehaviour
 
     public void ChangeSwag()
     {
-        hasSwag = !hasSwag;
+        /*hasSwag = !hasSwag;
         if (swag != null)
         {
             if (hasSwag)
                 swag.SetActive(true);
             else
                 swag.SetActive(false);
+        }*/
+
+        foreach (GameObject sweg in swagList)
+            if (sweg != null)
+                sweg.SetActive(false);
+
+        if (swagIndex < swagList.Length)
+        {
+            if (swagList[swagIndex] != null)
+                swagList[swagIndex].SetActive(true);
         }
+    }
+
+    public void IncrementSwag()
+    {
+        swagIndex++;
+        if (swagIndex > swagList.Length - 1)
+            swagIndex = 0;
+        ChangeSwag();
     }
 }
